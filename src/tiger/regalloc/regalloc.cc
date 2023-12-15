1258131e5a1237b261df2a6286095eee2a8aeaed
Bojun Ren <bj_Ren@outlook.com>
2023-12-15 20:32:41 +0000

    feat: skeleton for register allocator

--- src/tiger/regalloc/regalloc.cc
+++ src/tiger/regalloc/regalloc.cc
@@ -1,9 +1,181 @@
 #include "tiger/regalloc/regalloc.h"
 
 #include "tiger/output/logger.h"
+#include <functional>
+#include <sstream>
+
+#define NOT_EXIST_NODE                                                         \
+  throw std::runtime_error("the given node doesn't exist in the graph")
 
 extern frame::RegManager *reg_manager;
 
 namespace ra {
-/* TODO: Put your lab6 code here */
+
+std::size_t
+InterfereGraph::hash_temp_pair_(const BitMap_Underlying &temp_pair) {
+  auto [u, v] = temp_pair;
+  std::stringstream ss{};
+  ss << u->Int() << v->Int();
+  return std::hash<std::string>{}(ss.str());
+}
+
+void InterfereGraph::add_node(TempNode temp, bool is_precolored) {
+  if (precolored.count(temp) || initial.count(temp)) {
+    return;
+  }
+  if (is_precolored) {
+    precolored.insert(temp);
+  } else {
+    initial.insert(temp);
+    // FIXME necessary?
+    adjList_.emplace(std::make_pair(temp, TempNodeSet{}));
+    degree_.emplace(std::make_pair(temp, 0));
+  }
+}
+
+void InterfereGraph::add_edge(TempNode u, TempNode v) {
+  if (u != v && adjSet_.count(std::make_pair(u, v))) {
+    return;
+  }
+  adjSet_.insert(std::make_pair(u, v));
+  adjSet_.insert(std::make_pair(v, u));
+  if (!precolored.count(u)) {
+    adjList_[u].insert(v);
+    degree_[u]++;
+  }
+  if (!precolored.count(v)) {
+    adjList_[v].insert(u);
+    degree_[v]++;
+  }
+}
+
+std::size_t InterfereGraph::degree(TempNode v) const { return degree_.at(v); }
+
+auto InterfereGraph::adj_of(TempNode v) const -> const TempNodeSet & {
+  return adjList_.at(v);
+}
+
+bool RegAllocator::is_move_instr(assem::Instr *instr) {
+  return typeid(*instr) == typeid(assem::MoveInstr);
+}
+
+std::vector<std::tuple<TempNode, bool>>
+RegAllocator::extract_temps(assem::Instr *assem) {
+  std::vector<std::tuple<TempNode, bool>> res{};
+
+  auto move_instr = static_cast<assem::MoveInstr *>(assem);
+  auto dst = move_instr->Def()->GetList();
+  auto src = move_instr->Use()->GetList();
+  for (const auto temp : dst) {
+    res.emplace_back(temp, reg_manager->temp_map_->Look(temp) != nullptr);
+  }
+  for (const auto temp : src) {
+    res.emplace_back(temp, reg_manager->temp_map_->Look(temp) != nullptr);
+  }
+  return res;
+}
+
+void RegAllocator::RegAlloc() {
+  // TODO
+  livenessAnalysis();
+  build_interfere_graph();
+}
+
+void RegAllocator::livenessAnalysis() {
+  fg::FlowGraphFactory fg_factory{assem_instr_->GetInstrList()};
+  fg_factory.AssemFlowGraph();
+  flow_graph_.reset(fg_factory.GetFlowGraph());
+
+  live::LiveGraphFactory live_factory{flow_graph_.get()};
+  live_factory.Liveness();
+  liveness_.reset(live_factory.TransferResult());
+}
+
+void RegAllocator::build_interfere_graph() {
+  // In fact we can merge nodes into some basic blocks, which are nodes of the
+  // new graph. With basic blocks liveness analysis can be accelerated.
+  // Here we just treat each instruction as a node and a basic block, which
+  // makes things simple. See P223.
+  auto node_list = this->flow_graph_->Nodes()->GetList();
+
+  // Create all nodes. This step also initialize `initial` and `precolored` in
+  // the interfere graph.
+  for (const auto node : node_list) {
+    auto temps = extract_temps(node->NodeInfo());
+    for (const auto temp : temps) {
+      std::apply(std::bind(&InterfereGraph::add_node, this->interfere_graph_,
+                           std::placeholders::_1, std::placeholders::_2),
+                 temp);
+    }
+  }
+
+  for (auto block : node_list) {
+    temp::TempList live(
+        *liveness_->out(block)); // copied because it can be modified.
+    const auto instr = block->NodeInfo();
+
+    if (is_move_instr(instr)) {
+      auto move_instr = static_cast<assem::MoveInstr *>(instr);
+      live = temp_diff(live, *move_instr->Use());
+      assert(instr->Def()->GetList().size() == 1);
+      assert(instr->Use()->GetList().size() == 1);
+
+      {
+        auto def_and_use = temp_union(*move_instr->Def(), *move_instr->Use());
+        for (const auto temp : def_and_use.GetList()) {
+          // Automatically insert the set of instructions if it doesn't exist.
+          move_list[temp].insert(move_instr);
+        }
+      }
+      worklist_moves.insert(move_instr);
+
+      // Add interference edges.
+      live = temp_union(live, *move_instr->Def());
+      for (const auto temp_def : move_instr->Def()->GetList()) {
+        for (const auto temp_out : live.GetList()) {
+          this->interfere_graph_.add_edge(temp_def, temp_out);
+        }
+      }
+    }
+  }
+
+  this->initial = std::move(this->interfere_graph_.get_initial());
+  this->precolored = std::move(this->interfere_graph_.get_precolored());
+}
+
+void RegAllocator::make_worklist() {
+  for (auto temp : initial) {
+    if (interfere_graph_.degree(temp) >= KColors) {
+      spill_worklist.insert(temp);
+    } else if (move_related(temp)) {
+      freeze_worklist.insert(temp);
+    } else {
+      simplify_worklist.insert(temp);
+    }
+  }
+  initial.clear();
+}
+
+auto RegAllocator::node_moves(TempNode n) const -> MoveInstrSet {
+  // tmp := union active_moves and worklist_moves
+  MoveInstrSet tmp = set_union(active_moves, worklist_moves);
+
+  const auto &moves = move_list.at(n);
+
+  // intersect move_list[n] and tmp
+  auto res = set_intersect(move_list.at(n), tmp);
+  return res;
+}
+
+bool RegAllocator::move_related(TempNode n) const {
+  return !node_moves(n).empty();
+}
+
+TempNodeSet RegAllocator::adjacent(TempNode n) const {
+  // tmp := union select_stack coalesced_nodes
+  auto tmp = set_union(selected_stack.get_set(), coalesced_nodes);
+  auto res = set_diff(interfere_graph_.adj_of(n), tmp);
+  return res;
+}
+
 } // namespace ra
--- src/tiger/regalloc/regalloc.h
+++ src/tiger/regalloc/regalloc.h
@@ -8,6 +8,10 @@
 #include "tiger/liveness/liveness.h"
 #include "tiger/regalloc/color.h"
 #include "tiger/util/graph.h"
+#include <map>
+#include <set>
+#include <stack>
+#include <unordered_set>
 
 namespace ra {
 
@@ -26,8 +30,236 @@ public:
   ~Result();
 };
 
+using TempNode = temp::Temp const *;
+
+struct TempCmp {
+  bool operator()(TempNode lhs, TempNode rhs) const {
+    return lhs->Int() < rhs->Int();
+  }
+};
+
+struct TempEqualTo {
+  bool operator()(TempNode lhs, TempNode rhs) const {
+    return lhs->Int() == rhs->Int();
+  }
+};
+
+struct TempHash {
+  std::size_t operator()(TempNode temp) const { return temp->Int() - 100; }
+};
+
+using TempNodeSet = std::set<TempNode, TempCmp>;
+template <typename T> using TempNodeMap = std::map<TempNode, T, TempCmp>;
+
+template <typename T, typename Cmp>
+std::set<T, Cmp> set_union(const std::set<T, Cmp> &lhs,
+                           const std::set<T, Cmp> &rhs) {
+  std::set<T, Cmp> res{0, lhs.key_comp()};
+  std::set_union(lhs.begin(), lhs.end(), rhs.begin(), rhs.end(),
+                 std::inserter(res, res.begin()));
+  return res;
+}
+
+template <typename T, typename Cmp>
+std::set<T, Cmp> set_diff(const std::set<T, Cmp> &lhs,
+                          const std::set<T, Cmp> &rhs) {
+  std::set<T, Cmp> res{0, lhs.key_comp()};
+  std::set_difference(lhs.begin(), lhs.end(), rhs.begin(), rhs.end(),
+                      std::inserter(res, res.begin()));
+  return res;
+}
+
+template <typename T, typename Cmp>
+std::set<T, Cmp> set_intersect(const std::set<T, Cmp> &lhs,
+                               const std::set<T, Cmp> &rhs) {
+  std::set<T, Cmp> res{0, lhs.key_comp()};
+  std::set_intersection(lhs.begin(), lhs.end(), rhs.begin(), rhs.end(),
+                        std::inserter(res, res.begin()));
+  return res;
+}
+
+class InterfereGraph {
+public:
+  InterfereGraph() = default;
+
+  void add_node(TempNode temp, bool is_precolored = false);
+  void add_edge(TempNode u, TempNode v);
+  std::size_t degree(TempNode v) const;
+  const TempNodeSet &adj_of(TempNode v) const;
+
+  TempNodeSet &get_precolored() & { return precolored; }
+  TempNodeSet &get_initial() & { return initial; }
+
+private:
+  using BitMap_Underlying = std::pair<TempNode, TempNode>;
+
+  static std::size_t hash_temp_pair_(const BitMap_Underlying &temp_pair);
+
+  /**
+   * @brief The set of interference edges (u, v) in the graph.
+   * Tell if u and v are adjacent quickly.
+   *
+   * @note (u, v) in adjSet <=> (v, u) in adjSet.
+   */
+  std::unordered_set<BitMap_Underlying,
+                     decltype(&InterfereGraph::hash_temp_pair_)>
+      adjSet_{0, &InterfereGraph::hash_temp_pair_};
+  /**
+   * @brief Adjacency list representation of the graph.
+   * Get all the nodes adjacent to node X.
+   *
+   */
+  TempNodeMap<TempNodeSet> adjList_{};
+
+  /**
+   * @brief Current degree of each node.
+   *
+   * @note degree[n] == adjList[n].size isn't satisfied all the time.
+   *
+   */
+  TempNodeMap<std::size_t> degree_{};
+
+  /// Machine registers, preassigned a color.
+  TempNodeSet precolored{};
+  /// Temporary registers, neither precolored nor processed.
+  TempNodeSet initial{};
+};
+
 class RegAllocator {
-  /* TODO: Put your lab6 code here */
+  struct NodeAdditionalInfo {
+    // TODO
+  };
+
+public:
+  static constexpr int KColors = static_cast<int>(frame::Register::COUNT);
+
+  RegAllocator(frame::Frame *frame, std::unique_ptr<cg::AssemInstr> assem_instr)
+      : assem_instr_{std::move(assem_instr)}, frame_{frame}, result_{nullptr} {
+    // TODO
+  }
+
+  void RegAlloc();
+
+  std::unique_ptr<ra::Result> TransferResult() { return std::move(result_); }
+
+private:
+  using MoveInstr = assem::MoveInstr *;
+  using MoveInstrSet = std::set<MoveInstr>;
+
+  // TODO
+
+  // Input.
+  frame::Frame *frame_{};
+  std::unique_ptr<cg::AssemInstr> assem_instr_{};
+
+  // Liveness products.
+  std::unique_ptr<fg::FGraph> flow_graph_{};
+  std::unique_ptr<live::LivenessResult> liveness_{};
+
+  /**
+   * @brief Node work-lists, sets, and stacks.
+   * The following lists and sets are always mutually disjoint and every node is
+   * always in exactly one of the sets or lists
+   *
+   */
+
+  /// Machine registers, preassigned a color.
+  TempNodeSet precolored{};
+  /// Temporary registers, neither precolored nor processed.
+  TempNodeSet initial{};
+  /// low-degree non-move-related nodes.
+  TempNodeSet simplify_worklist{};
+  /// low-degree move-related nodes.
+  TempNodeSet freeze_worklist{};
+  /// high-degree nodes.
+  TempNodeSet spill_worklist{};
+  TempNodeSet spilled_nodes{};
+  TempNodeSet coalesced_nodes{};
+  TempNodeSet colored_nodes{};
+  struct {
+    const TempNodeSet &get_set() const & { return set_; }
+    void push(TempNode n) {
+      set_.insert(n);
+      stack_.push(n);
+    }
+    TempNode pop() {
+      auto n = stack_.top();
+      stack_.pop();
+      set_.erase(n);
+      return n;
+    }
+
+  private:
+    TempNodeSet set_{};
+    std::stack<TempNode> stack_{};
+  } selected_stack{};
+  // TempNodeSet select_stack{};
+
+  /**
+   * @brief Move sets.
+   * There are five sets of move instructions, and every move is in exactly one
+   * of these sets (after Build through the end of Main)
+   */
+
+  /// Moves that have been coalesced.
+  MoveInstrSet coalesced_moves{};
+  /// Moves whose source and target interfere.
+  MoveInstrSet constrained_moves{};
+  /// Moves that will no longer be considered for coalescing.
+  MoveInstrSet frozen_moves{};
+  /// Moves enabled for possible coalescing.
+  MoveInstrSet worklist_moves{};
+  /// Moves not yet ready for coalescing.
+  MoveInstrSet active_moves{};
+
+  /**
+   * @brief Other data structures.
+   *
+   */
+
+  /// adjSet, adjList, degree
+  InterfereGraph interfere_graph_{};
+
+  /// A mapping from a node to the list of moves it is associated with.
+  TempNodeMap<MoveInstrSet> move_list{};
+
+  // Final result.
+  std::unique_ptr<ra::Result> result_{};
+
+  /**
+   * @brief Basic workflow.
+   *
+   */
+
+  void livenessAnalysis();
+  void build_interfere_graph();
+  void make_worklist();
+
+  /**
+   * @brief Helper functions.
+   *
+   */
+
+  /// Get all valid moves related to n.
+  MoveInstrSet node_moves(TempNode n) const;
+  bool move_related(TempNode n) const;
+  /// Get all actual adjacent node for n.
+  TempNodeSet adjacent(TempNode n) const;
+
+  /**
+   * @brief Other functions.
+   *
+   */
+
+  /// Whether the instruction is a move instruction.
+  static bool is_move_instr(assem::Instr *);
+  /**
+   * @brief Extract temps from the instruction.
+   *
+   * @return std::vector<std::tuple<TempNode, bool>> A list of temporaries. The
+   * second value indicates whether the temporary register is precolored.
+   */
+  static std::vector<std::tuple<TempNode, bool>> extract_temps(assem::Instr *);
 };
 
 } // namespace ra

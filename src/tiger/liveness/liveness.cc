f59e8d993a878bc9d54ee87adf3eac550616b688
Bojun Ren <bj_Ren@outlook.com>
2023-12-15 13:07:11 +0000

    feat: liveness analysis
    Finally I decide to do only one thing in the liveness module - create `out` and `in` for each block.

--- src/tiger/liveness/liveness.cc
+++ src/tiger/liveness/liveness.cc
@@ -43,13 +43,79 @@ MoveList *MoveList::Intersect(MoveList *list) {
   return res;
 }
 
-void LiveGraphFactory::LiveMap() { /* TODO: Put your lab6 code here */ }
+void LiveGraphFactory::LiveMap() {
+  auto node_list = this->flowgraph_->Nodes()->GetList();
 
-void LiveGraphFactory::InterfGraph() { /* TODO: Put your lab6 code here */ }
+  // Initialize
+  for (auto node : node_list) {
+    this->in_->Enter(node, new temp::TempList{});
+    this->out_->Enter(node, new temp::TempList{});
+  }
 
-void LiveGraphFactory::Liveness() {
-  LiveMap();
-  InterfGraph();
+  bool has_change = false;
+  do {
+    // Reverse
+    for (auto it = rbegin(node_list); it != rend(node_list); ++it) {
+      auto node = *it;
+      auto instr = node->NodeInfo();
+      auto cur_in = this->in_->Look(node);
+      auto cur_out = this->out_->Look(node);
+      assert(cur_in && cur_out);
+
+      // out[n] = U in[s] for s in succ[n]
+      {
+        temp::TempList dest{};
+
+        for (auto s : node->Succ()->GetList()) {
+          auto s_out = this->get_out_(s);
+          dest = temp_union(dest, *s_out);
+        }
+
+        if (dest.GetList().size() > cur_out->GetList().size()) {
+          has_change = true;
+        }
+
+        cur_out->swap(dest);
+      }
+
+      // in[n] = use[n] U (out[n] \ def[n])
+      {
+        temp::TempList kill(*instr->Def());
+        kill.sort();
+        auto diff_set = temp_diff(*cur_out, kill);
+
+        temp::TempList gen(*instr->Use());
+        gen.sort();
+        auto dest = temp_union(gen, diff_set);
+
+        assert(dest.GetList().size() >= cur_in->GetList().size());
+        if (dest.GetList().size() > cur_in->GetList().size()) {
+          has_change = true;
+        }
+
+        cur_in->swap(dest);
+      }
+    }
+
+  } while (has_change);
 }
+
+void LiveGraphFactory::InterfGraph() { assert(0); }
+
+temp::TempList *
+LiveGraphFactory::get_out_(graph::Node<assem::Instr> *node) const noexcept {
+  auto res = this->out_->Look(node);
+  assert(res);
+  return res;
+}
+
+temp::TempList *
+LiveGraphFactory::get_in_(graph::Node<assem::Instr> *node) const noexcept {
+  auto res = this->in_->Look(node);
+  assert(res);
+  return res;
+}
+
+void LiveGraphFactory::Liveness() { LiveMap(); }
 
 } // namespace live
--- src/tiger/liveness/liveness.h
+++ src/tiger/liveness/liveness.h
@@ -16,12 +16,12 @@ using INodeListPtr = graph::NodeList<temp::Temp> *;
 using IGraph = graph::Graph<temp::Temp>;
 using IGraphPtr = graph::Graph<temp::Temp> *;
 
-class MoveList {
+class [[deprecated]] MoveList {
 public:
   MoveList() = default;
 
-  [[nodiscard]] const std::list<std::pair<INodePtr, INodePtr>> &
-  GetList() const {
+  [[nodiscard]] const std::list<std::pair<INodePtr, INodePtr>> &GetList()
+      const {
     return move_list_;
   }
   void Append(INodePtr src, INodePtr dst) { move_list_.emplace_back(src, dst); }
@@ -30,8 +30,8 @@ public:
   void Prepend(INodePtr src, INodePtr dst) {
     move_list_.emplace_front(src, dst);
   }
-  MoveList *Union(MoveList *list);
-  MoveList *Intersect(MoveList *list);
+  MoveList *Union(MoveList * list);
+  MoveList *Intersect(MoveList * list);
 
 private:
   std::list<std::pair<INodePtr, INodePtr>> move_list_;
@@ -41,15 +41,42 @@ private:
  * @brief The result of liveness analysis.
  *
  */
-struct LiveGraph {
+struct [[deprecated]] LiveGraph {
   /// An undirected graph whose edges connect variables that interfere.
   IGraphPtr interf_graph;
   /// A list of node-pairs that should be assigned the same register if
   /// possible.
   MoveList *moves;
 
-  LiveGraph(IGraphPtr interf_graph, MoveList *moves)
+  LiveGraph(IGraphPtr interf_graph, MoveList * moves)
       : interf_graph(interf_graph), moves(moves) {}
+};
+
+struct LivenessResult {
+public:
+  LivenessResult() = default;
+  LivenessResult(
+      std::unique_ptr<graph::Table<assem::Instr, temp::TempList>> in,
+      std::unique_ptr<graph::Table<assem::Instr, temp::TempList>> out)
+      : in_(std::move(in)), out_(std::move(out)) {}
+
+  LivenessResult(const LivenessResult &result) = delete;
+  LivenessResult(LivenessResult &&result) = delete;
+
+  temp::TempList *out(graph::Node<assem::Instr> *node) const noexcept {
+    auto res = this->out_->Look(node);
+    assert(res);
+    return res;
+  }
+  temp::TempList *in(graph::Node<assem::Instr> *node) const noexcept {
+    auto res = this->in_->Look(node);
+    assert(res);
+    return res;
+  }
+
+private:
+  std::unique_ptr<graph::Table<assem::Instr, temp::TempList>> in_;
+  std::unique_ptr<graph::Table<assem::Instr, temp::TempList>> out_;
 };
 
 class LiveGraphFactory {
@@ -60,30 +87,31 @@ public:
    * @param flowgraph
    */
   explicit LiveGraphFactory(fg::FGraphPtr flowgraph)
-      : flowgraph_(flowgraph), live_graph_(new IGraph(), new MoveList()),
+      : flowgraph_(flowgraph),
         in_(std::make_unique<graph::Table<assem::Instr, temp::TempList>>()),
-        out_(std::make_unique<graph::Table<assem::Instr, temp::TempList>>()),
-        temp_node_map_(new tab::Table<temp::Temp, INode>()) {}
+        out_(std::make_unique<graph::Table<assem::Instr, temp::TempList>>()) {}
   /**
    * @brief Trigger liveness analysis.
    *
-   * The result can be retrieved via #GetLiveGraph.
    */
   void Liveness();
-  LiveGraph GetLiveGraph() { return live_graph_; }
-  tab::Table<temp::Temp, INode> *GetTempNodeMap() { return temp_node_map_; }
+
+  LivenessResult *TransferResult() {
+    return new LivenessResult{std::move(in_), std::move(out_)};
+  }
 
 private:
+  // Input: the flow graph
   fg::FGraphPtr flowgraph_;
-  LiveGraph live_graph_;
 
   std::unique_ptr<graph::Table<assem::Instr, temp::TempList>> in_;
   std::unique_ptr<graph::Table<assem::Instr, temp::TempList>> out_;
-  /// Map temporary to the graph node
-  tab::Table<temp::Temp, INode> *temp_node_map_;
 
   void LiveMap();
-  void InterfGraph();
+  [[deprecated]] void InterfGraph();
+
+  temp::TempList *get_out_(graph::Node<assem::Instr> *node) const noexcept;
+  temp::TempList *get_in_(graph::Node<assem::Instr> *node) const noexcept;
 };
 
 } // namespace live
